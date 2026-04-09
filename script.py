17b43fb6b64f2cb1c119ad37eab6f27bd0b69cd7
root <root@labvm.vm>
2026-04-09 18:51:20 +0000

    Evaluacion N°1 DRY7122

---
+++ script.py
@@ -0,0 +1,5 @@
+print ("Evaluacion N°1 Programacion y Redes Virtualizadas")
+
+print("Inregrantes:")
+print("CRISTOBAL AMAYO")
+print("IGNACIO BUSTAMANTE")
---
+++ script2.py
@@ -0,0 +1,7 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+codigo = input("Ingrese su codigo-seccion: ")
+sede = input("Ingrese su sed: ")
+
+print("\n--- Datos ingresados ---")
+print (nombre, apellido, codigo, sede)
